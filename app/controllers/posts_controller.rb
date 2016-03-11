a185ff8ad2f28b76394d1162fc54a25b61f536c0
Jonathan Miller <crashaftertouch@gmail.com>
2016-03-11 13:00:37 -0500

    Dynamically added name of user to each post, and ensured that only the user who owns a post can edit it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   # Set current post for show, edit, update, and delete methods
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
 
   # Display all posts on the index page
@@ -14,11 +15,13 @@ class PostsController < ApplicationController
 
   # CRUD methods for posts
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
@@ -60,5 +63,15 @@ class PostsController < ApplicationController
   # Helper method to assign the current post to an ivar
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  # Helper method to ensure a user cannot edit a post that's not their own
+  # This method ensures that navigating to /posts/#idnumber/edit with an account
+  # that is not the owner of the post will not be able to edit that post.
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 end
